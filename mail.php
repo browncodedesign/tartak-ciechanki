919500a17d5ee1410d6e206fba4cf1ae81657351
Alan Mazurek <37226652+browncodedesign@users.noreply.github.com>
2018-03-20 13:58:11 +0100

    Add files via upload
    The first, done scene of this website which featured on the internet.

--- mail.php
+++ mail.php
@@ -1,24 +1,22 @@
 
 
 <!DOCTYPE html>
-<html lng="pl"
+<html langng="pl"
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		
-
 			<title>Tartak ciechanki</title>
+			<!---STYLE-->
 				<link rel="stylesheet" href+"normalize.css">
 				<link rel="stylesheet" href="style.css">
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic&amp;subset=latin-ext">
-				
-		<head>	
+				<link href="https://fonts.googleapis.com/css?family=Khand|Orbitron" rel="stylesheet">
+		</head>	
 	<body>
-		<header class="header" style="min-height: 90vh;">
+		<header class="header echo" style="min-height: 90vh;">
 				<h1>Tartak ciechanki</h1>
-				
+			<!--- PHP SCRIPT --->
 				<?php
-				$to      = 'amazurek92@gmail.com';
+				$to      = 'drewno.leczna@wp.pl';
 				$name    = $_POST['name'];
 				$email   = $_POST['email'];
 				$phone   = $_POST['phone'];
@@ -29,11 +27,10 @@
 				$headers = 'From: ' . $name . ' (' . $email . ') ' . ' (' . $phone .  ')';
 				$headers .= 'Content-Type: Tartak-ciechanki.pl; charset=utf-8';
 				mail($to, $subject, $message, $headers);
-				
-				
-				echo '<h2>Wiadomość wysłana</h1>'
 			?>
-			<br>
+			<!--- PHP SCRIPT  ENDS--->
+		<p>Wiadomość wysłana pomyślnie</p>
+			<br><br>
 			<div>
 				<ul class="nav" style="width: 300px; margin: auto" >
 				<li><a href="index.html">Powrót do strony głównej</a></li>
@@ -43,7 +40,6 @@
 	</header>
 		<footer class="footer">
 			<hr>
-			<p>&copy; Alan 2019</p>
-		</footer>
+			<p>&copy; 2018 by Alan Mazurek   | All rights reserved</p>
 		</body>
 	</html>
